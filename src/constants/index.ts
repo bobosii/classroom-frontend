ce7e10bc961b0557a454b8f54df867ad2cbf1f17
bobosii <day.emir@hotmail.com>
2026-01-21 08:24:44 +0300

    data provider implemented
    Signed-off-by: bobosii <day.emir@hotmail.com>

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const DEPARTMENTS = [
+    'Computer science',
+    'Math',
+    'English'
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+    value: dept,
+    label: dept
+}))
