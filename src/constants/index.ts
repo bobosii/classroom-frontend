0d63a72b8ccedb219946e560e8d86e3693916e03
bobosii <day.emir@hotmail.com>
2026-01-27 09:18:35 +0300

    implement cloudinary image upload
    Signed-off-by: bobosii <day.emir@hotmail.com>

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,27 +1,33 @@
 export const DEPARTMENTS = ["Computer science", "Math", "English"];
 
 export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
-  value: dept,
-  label: dept,
+    value: dept,
+    label: dept,
 }));
 
 export const MAX_FILE_SIZE = 3 * 1024 * 1024; // 3mb
-export const ALLOWED_TYPES = [
-  "image/png",
-  "image/jpeg",
-  "image/jpg",
-  "image/webp",
-];
+export const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
 
-export const CLOUDINARY_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_UPLOAD_URL;
-export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
-export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
+// Validator for environment variables
+const getEnvVar = (key: string) => {
+    const value = import.meta.env[key];
+    if (!value) {
+        throw new Error(`Missing Environment variable ${key}`);
+    }
+    return value;
+};
 
+// Cloudinary
+export const CLOUDINARY_UPLOAD_URL = getEnvVar("VITE_CLOUDINARY_UPLOAD_URL");
+export const CLOUDINARY_CLOUD_NAME = getEnvVar("VITE_CLOUDINARY_CLOUD_NAME");
+export const CLOUDINARY_UPLOAD_PRESET = getEnvVar("VITE_CLOUDINARY_UPLOAD_PRESET");
+
+// Backend
+export const BACKEND_BASE_URL = getEnvVar("VITE_BACKEND_BASE_URL");
 export const BASE_URL = import.meta.env.VITE_API_URL;
+
+// Auth storage keys
 export const ACCESS_TOKEN_KEY = import.meta.env.VITE_ACCESS_TOKEN_KEY;
 export const REFRESH_TOKEN_KEY = import.meta.env.VITE_REFRESH_TOKEN_KEY;
 
 export const REFRESH_TOKEN_URL = `${BASE_URL}/refresh-token`;
-
-export const CLOUDINARY_UPLOAD_PRESET = import.meta.env
-  .VITE_CLOUDINARY_UPLOAD_PRESET;
